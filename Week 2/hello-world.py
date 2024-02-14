af11b1f5bd69450e54076a9b3d10ad67bb39a60d
Rahul9315 <baghel931515@gmail.com>
2024-02-14 19:48:33 +0000

    Added Average function to fast_forward-test branch

--- Week 2/hello-world.py
+++ Week 2/hello-world.py
@@ -1,6 +1,11 @@
 print("This is another String!")
 print("welcome to codinorld\n")
-print("For Loop Starts Here until 10")
+print("For Loop Starts Here until 2 - 15")
 
-for i in range (1,11):
-    print(i)
+result = 0
+
+for i in range (2,15):
+    result = i + result
+    length = i
+
+print("average :: ", result/length)
